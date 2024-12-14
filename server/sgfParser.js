713b7b06120cdcebdd76fd7d13df2a8d75a3369d
Jurara12 <jurgenarayaseas@gmail.com>
2024-12-14 17:30:26 +0000

    sgf update

--- server/sgfParser.js
+++ server/sgfParser.js
@@ -1,23 +1,23 @@
-const sgfParse = require('sgf-parse'); // This is the library for parsing SGF files
+const SmartGame = require('smartgame'); // Use smartgame for SGF parsing
 
 // Function to parse SGF content and extract puzzle data
 function parseSGFData(sgfContent) {
-  const sgfTree = sgfParse(sgfContent);
-  const rootNode = sgfTree.nodes[0];
+  const game = SmartGame.parse(sgfContent); // Parse SGF file
+  const rootNode = game.nodes[0]; // Get the root node of the game
 
-  const boardSize = rootNode.SZ ? parseInt(rootNode.SZ[0], 10) : 19;
+  const boardSize = rootNode.SZ || 19;
 
   // Extract initial positions (AB for black stones, AW for white stones)
   const initialPosition = {
-    B: rootNode.AB ? rootNode.AB.map(coord => coord) : [],
-    W: rootNode.AW ? rootNode.AW.map(coord => coord) : [],
+    B: rootNode.AB || [],
+    W: rootNode.AW || [],
   };
 
-  // Extract solution lines (the first variation of moves in the SGF)
-  const solutionLines = extractSolutionLines(sgfTree);
+  // Extract solution lines (variations of moves in the SGF)
+  const solutionLines = extractSolutionLines(game);
 
-  const title = rootNode.GN ? rootNode.GN[0] : 'Untitled Puzzle';
-  const difficulty = 'Intermediate'; // Can be set dynamically based on SGF metadata
+  const title = rootNode.GN || 'Untitled Puzzle';
+  const difficulty = 'Intermediate'; // You can adjust this dynamically
 
   return {
     boardSize,
@@ -29,24 +29,26 @@ function parseSGFData(sgfContent) {
 }
 
 // Extract solution lines from the SGF tree
-function extractSolutionLines(sgfTree) {
-  if (!sgfTree.children || sgfTree.children.length === 0) {
+function extractSolutionLines(game) {
+  if (!game || !game.nodes || game.nodes.length === 0) {
     return [];
   }
 
   const solution = [];
-  function traverse(node) {
-    if (node.nodes[0].B) {
-      solution.push({ color: 'B', move: node.nodes[0].B[0] });
-    } else if (node.nodes[0].W) {
-      solution.push({ color: 'W', move: node.nodes[0].W[0] });
-    }
-    if (node.children && node.children[0]) {
-      traverse(node.children[0]);
-    }
+  function traverse(nodes) {
+    nodes.forEach(node => {
+      if (node.B) {
+        solution.push({ color: 'B', move: node.B });
+      } else if (node.W) {
+        solution.push({ color: 'W', move: node.W });
+      }
+      if (node.children) {
+        traverse(node.children);
+      }
+    });
   }
 
-  traverse(sgfTree.children[0]);
+  traverse(game.nodes);
   return solution;
 }
 
